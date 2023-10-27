c577df002695c25da11887fa666fbfb53ab2e3e8
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-10-26 20:30:48 -0500

    live plots

--- myo.py
+++ myo.py
@@ -3,12 +3,30 @@
 import multiprocessing
 from pyomyo import Myo, emg_mode
 import csv
-from serial.tools.list_ports import comports
+from bokeh.plotting import figure, curdoc
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import gridplot
+from bokeh.io import push_notebook
+import time
 
 worker_q = multiprocessing.Queue()
 csv_q = multiprocessing.Queue()
 graph_q = multiprocessing.Queue()
 
+emg_plots = [figure(title=f'EMG Channel {i+1}', width=400, height=200, 
+                    tools='', toolbar_location=None) for i in range(8)]
+
+emg_sources = [ColumnDataSource(data=dict(x=[], y=[])) for _ in range(8)]
+
+for i, plot in enumerate(emg_plots):
+    plot.line('x', 'y', source=emg_sources[i], line_width=2)
+    plot.x_range.follow = "end"
+    plot.x_range.follow_interval = 100  # Adjust as needed for your data rate
+
+grid = gridplot(emg_plots, ncols=4)
+
+curdoc().add_root(grid)
+curdoc().title = "Real-time EMG Data"
 
 def worker(q):
     m = Myo(mode=emg_mode.FILTERED)
@@ -40,11 +58,29 @@ def process_to_csv(q, csvfile):
             emg = list(q.get())
             writer.writerow(emg)
 
+
 def process_to_graph(q):
+    # Initialize data dictionary for each channel
+    emg_data = {i: {'x': [], 'y': []} for i in range(8)}
+    line_indices = [emg_sources[i].data['x'] for i in range(8)]
+
     while True:
         while not q.empty():
             emg = list(q.get())
-            # do something
+
+            for i in range(8):
+                emg_data[i]['x'].append(len(emg_data[i]['x']) + 1)
+                emg_data[i]['y'].append(emg[i])
+
+                # Trim data to keep a fixed window size
+                if len(emg_data[i]['x']) > 100:  # Adjust window size as needed
+                    emg_data[i]['x'] = emg_data[i]['x'][1:]
+                    emg_data[i]['y'] = emg_data[i]['y'][1:]
+
+                # Update the data source for the Bokeh plot
+                emg_sources[i].data = emg_data[i]
+
+            time.sleep(0.01)  # Adjust the sleep duration based on your data rate
 
 if __name__ == '__main__':
     csvfile = open('myo_emg_data.csv', 'w+', newline='')
