cdcfc6f75bf0e6aa0b75e9342cc82eab07952761
zacharybsmartt <zacharybsmartt@gmail.com>
2024-09-29 11:55:29 -0500

    pushing

--- ModelTraining.py
+++ ModelTraining.py
@@ -1,36 +1,50 @@
+import numpy as np
+import pandas as pd
 import tensorflow as tf
-import tensorflow_datasets as tfds
-# Define your model function
-def create_emg_model(input_shape, num_classes):
-    model = tf.keras.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(num_classes, activation='softmax')  
-    ])
-    return model
+import tensorflow_decision_forests as tfdf
+from sklearn.model_selection import train_test_split
+import ydf
 
-train_ds = tfds.load('Gail Rest.xlsx', split = 'train[:80%]')
-test_ds = tfds.load('Gail Rest.xlsx', split = 'test[:20%]')
+training_data_path = '/home/zachary/Downloads/TURTLE/OLSN/olsn/Compiled and Labelled - Sheet1.csv'
+df = pd.read_csv(training_data_path)
 
-print(train_ds)
-# # Define input shape and number of classes
-# input_shape = (8,)
-# num_classes = 1
+train_df = df.sample(frac = 0.8)
+test_df = df.drop(train_df.index)
 
-# # Create the model
-# model = create_emg_model(input_shape, num_classes)
+train_df.size / 9 #number of elements split into training
+test_df.size / 9#number of elements split into testing
+(train_df.size / 9) + (test_df.size / 9) #total number of elements
 
-# # Compile the model
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_df, label="Class")
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df, label="Class")
 
-# # Train the model
-# model.fit(train_ds, test_ds, epochs=10, batch_size=32, validation_data=(train_ds, test_ds))
+model = tfdf.keras.RandomForestModel()
+model.fit(train_ds)
 
-# # Evaluate the model
-# test_loss, test_acc = model.evaluate(train_ds, test_ds)
-# print('Test accuracy:', test_acc)
+model.evaluate(test_ds)
 
-# # Save the model
-# model.save('emg_model.h5')
+model.predict(test_ds)
+
+results = model.evaluate(test_ds)
+
+model.save_weights('/home/zachary/Downloads/TURTLE/OLSN/olsn/model_weights.keras')
+
+
+
+
+"""
+porting model to harware below
+
+from sklearn.datasets import load_iris
+from everywhereml.sklearn.ensemble import RandomForestClassifier
+
+X, y = load_iris(return_X_y=True) # replace this with your own data!
+clf = RandomForestClassifier(n_estimators=5).fit(X, y)
+
+'''
+  Now we convert the classifier to C++ with a single line of code
+  - instance_name will create an instance of the classifier in the produced code
+    (you will use this name later)
+'''
+print(clf.to_arduino(instance_name='irisClassifier'))
+"""
