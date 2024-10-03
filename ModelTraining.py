2dc8c03f1cb61cea6fdf1135d45ffc676941f21d
zacharybsmartt <zacharybsmartt@gmail.com>
2024-10-02 19:46:58 -0500

    jaf push

--- ModelTraining.py
+++ ModelTraining.py
@@ -5,7 +5,7 @@ import tensorflow_decision_forests as tfdf
 from sklearn.model_selection import train_test_split
 import ydf
 
-training_data_path = '/home/zachary/Downloads/TURTLE/OLSN/olsn/Compiled and Labelled - Sheet1.csv'
+training_data_path = '/home/zachary/Downloads/TURTLE/OLSN/olsn/Compiled and Labelled - Sheet1.csv' # change this path 
 df = pd.read_csv(training_data_path)
 
 train_df = df.sample(frac = 0.8)
@@ -28,24 +28,5 @@ model.predict(test_ds)
 
 results = model.evaluate(test_ds)
 
-model.save_weights('/home/zachary/Downloads/TURTLE/OLSN/olsn/model_weights.keras')
+model.save_weights('/home/zachary/Downloads/TURTLE/OLSN/olsn/model_weights.keras') # change this path 
 
-
-
-
-"""
-porting model to harware below
-
-from sklearn.datasets import load_iris
-from everywhereml.sklearn.ensemble import RandomForestClassifier
-
-X, y = load_iris(return_X_y=True) # replace this with your own data!
-clf = RandomForestClassifier(n_estimators=5).fit(X, y)
-
-'''
-  Now we convert the classifier to C++ with a single line of code
-  - instance_name will create an instance of the classifier in the produced code
-    (you will use this name later)
-'''
-print(clf.to_arduino(instance_name='irisClassifier'))
-"""
